678c20343d02d169717ac125df2d156be8000345
TyrannousHail03 <TyrannousHail03@users.noreply.github.com>
2018-02-17 10:11:37 -0500

    Quiz Generator Version 1
    Version 1.1.0

--- Quiz Generator Version 1.py
+++ Quiz Generator Version 1.py
@@ -4,10 +4,12 @@ import sys  # To exit later in program
 fileName = input("What would you like your file to be named?")  # Takes user input for file name
 createFile = open(fileName + ".py", "w+")  # Creates a .py file in the directory this file is from
 
+# Warns the user of using " or ' punctuation in their answers
+print("Warning: This program will not function correctly if you use the ' symbol, use \ before the symbol. or ")
+
 # The following will write the base code for a quiz
 fileEdit = createFile
 questionNumber = 1  # Starts off the question number at 1
-
 
 def questionNumberPlus():
     global questionNumber
@@ -24,7 +26,7 @@ def questionGenerator(): # Generates Questions
                        + "else:\n" + "    print('Wrong')\n")
         questionNumberPlus()
         questionGenerator()
-    elif startGen.lower() == "no" or startGen == "n":
+    elif startGen.lower() == "no" or startGen.lower() == "n":
         sureExit = input("Are you sure you want to exit?")
         if sureExit.lower() == "no" or sureExit.lower() == "n":
             questionGenerator()
@@ -34,14 +36,9 @@ def questionGenerator(): # Generates Questions
         print("Sorry, your input was not understood.")
         questionGenerator()
 
-
 questionGenerator()
 
-# Minor things to add:
-#       1. Warnings on punctuation issues. - Do not use ' in a question
-# Major things to add:
-#       1. Multiple Answer Function and change questionGenerator to be 1 answer function
-#       2. Quiz Scorer with grades - give option for letter descriptors and number scale and both
-#       3. Enable users to quit programs by saying "quit" or "exit" (simple solution is add another if statement
-#       4. Give score report and tell users to type something when finished - will have to be specific to one keyword
-#           - Will have to make practice quiz to test out features to program within this program.
+# 1.1.0 Release Notes: Added print("The program will now exit.") when user inputs  no  for adding another question.
+# Added a quick check to see if users actually want to exit
+# Added a warning about using ' in the questions
+# Fixed bug where N as input for the exit warning would not be valid input
